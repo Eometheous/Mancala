25ac9534c4932514b55a000008c2f17b8d7ae75e
Eometheous <jonathan.sthomas@icloud.com>
2023-04-10 16:01:18 -0700

    Added Model

---
+++ src/main/java/main/Model.java
@@ -0,0 +1,33 @@
+package main;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.util.ArrayList;
+
+/**
+ * This is the model of the MVC model. It holds data and
+ * listeners so that the data can be viewed in many ways.
+ * @param <T> The type of data that will be stored
+ * @author Jonathan Stewart Thomas
+ */
+public class Model <T> {
+    private final ArrayList<T> dataList;
+    private final ArrayList<ChangeListener> listeners;
+    public Model() {
+        dataList = new ArrayList<>();
+        listeners = new ArrayList<>();
+    }
+    public void add(T data) {
+        dataList.add(data);
+    }
+    public void attach(ChangeListener listener) {
+        listeners.add(listener);
+    }
+    public void update(int position, T updated) {
+        dataList.set(position, updated);
+        for (ChangeListener listener : listeners) {
+            listener.stateChanged(new ChangeEvent(this));
+        }
+    }
+    public T get(int index) {return dataList.get(index);}
+}
