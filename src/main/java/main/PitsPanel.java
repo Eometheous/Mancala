0fd29b5c4348930c3af52dc0dee633e7985489af
kelly <kelly.dang70@gmail.com>
2023-04-13 11:02:26 -0700

    Added Javadocs

--- src/main/java/main/PitsPanel.java
+++ src/main/java/main/PitsPanel.java
@@ -3,6 +3,11 @@ package main;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * This class stores and displays the 12 pits for the Mancala Board game in a grid layout.
+ * @author Kelly Dang
+ * @version 1.0.0.230413
+ */
 public class PitsPanel extends JPanel {
     public PitsPanel(Model<Integer> m) {
         setLayout(new GridLayout(2, 6));
