f389acdb1725e3f39ac42ba8da23800617bf9d09
Eometheous <jonathan.sthomas@icloud.com>
2023-04-17 15:23:54 -0700

    Added JavaDoc Comments

--- src/main/java/main/BeadPainter.java
+++ src/main/java/main/BeadPainter.java
@@ -3,7 +3,19 @@ package main;
 import java.awt.*;
 import java.util.ArrayList;
 
+/**
+ * Paints BeadIcons
+ * @author Jonathan Stewart Thomas
+ * @version 1.0.0.230417
+ */
 public class BeadPainter extends BeadIcon{
+    /**
+     * Paints BeadIcons with constraints for the x-axis
+     * @param beads         the beads being painted
+     * @param c             the component the beads are a part of
+     * @param g             the graphics to paint the beads
+     * @param constraints   the x-axis constraints
+     */
     public static void paintBeads(ArrayList<BeadIcon> beads, Component c, Graphics g, int constraints) {
         int x = 15, y = 15;
         for (BeadIcon bead : beads) {
