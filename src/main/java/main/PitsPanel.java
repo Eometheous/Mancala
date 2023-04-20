59a862b3ee2a594d5af4c67de6109fec1a14f428
kelly <kelly.dang70@gmail.com>
2023-04-20 09:37:29 -0700

    Added Javadocs

--- src/main/java/main/PitsPanel.java
+++ src/main/java/main/PitsPanel.java
@@ -10,7 +10,7 @@ import java.awt.*;
  */
 public class PitsPanel extends JPanel {
     public PitsPanel(Model<Integer> m) {
-        setLayout(new GridLayout(2, 6));
+        setLayout(new GridLayout(2, 6, 25, 25));
         for(int i = 0; i < 12; i++) {
             add(new Pit(m, i));
         }
