808ed10b257edc67aec439ab3de06923aa576fdc
Eometheous <jonathan.sthomas@icloud.com>
2023-04-26 09:50:49 -0700

    removed unused import

--- src/main/java/main/MancalaPit.java
+++ src/main/java/main/MancalaPit.java
@@ -2,8 +2,7 @@ package main;
 import javax.swing.event.*;
 import java.util.ArrayList;
 import javax.swing.JPanel;
-import java.awt.*; 
-import java.awt.event.*;
+import java.awt.*;
 import java.awt.geom.*;
 
 /**
