74874ace532b7c8baf7745c96ba0a915ddda6af6
Eometheous <jonathan.sthomas@icloud.com>
2023-04-25 17:49:56 -0700

    Added the default board style

---
+++ src/main/java/main/DefaultBoardStyle.java
@@ -0,0 +1,26 @@
+package main;
+
+import java.awt.*;
+
+public class DefaultBoardStyle implements BoardStyle{
+    Color boardColor, mancalaPitColor, pitColor;
+    public DefaultBoardStyle() {
+        boardColor = new Color(163, 114, 46);
+        mancalaPitColor = new Color(204, 151, 78);
+        pitColor = new Color(133, 91, 17);
+    }
+    @Override
+    public Color setMancalaPitColor() {
+        return mancalaPitColor;
+    }
+
+    @Override
+    public Color setPitColor() {
+        return pitColor;
+    }
+
+    @Override
+    public Color setBoardColor() {
+        return boardColor;
+    }
+}
