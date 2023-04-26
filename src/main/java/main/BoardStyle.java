de632dd5a1fc9abd45407f31e9a0f5027a72788d
Eometheous <jonathan.sthomas@icloud.com>
2023-04-26 09:43:02 -0700

    Added JavaDocs

--- src/main/java/main/BoardStyle.java
+++ src/main/java/main/BoardStyle.java
@@ -3,7 +3,9 @@ package main;
 import java.awt.*;
 
 /**
- *
+ * An interface for setting the colors of a mancala board
+ * @author Jonathan Stewart Thomas
+ * @version 1.0.0.230426
  */
 public interface BoardStyle {
     Color setMancalaPitColor();
--- src/main/java/main/BoardStylePicker.java
+++ src/main/java/main/BoardStylePicker.java
@@ -2,22 +2,41 @@ package main;
 
 import java.awt.*;
 
+/**
+ * A board style picker which follows the strategy pattern.
+ * @author Jonathan Stewart Thomas
+ * @version 1.0.0.230426
+ */
 public class BoardStylePicker implements BoardStyle{
     BoardStyle style;
+
+    /**
+     * Initializes this with a specific style
+     * @param style the style to use
+     */
     BoardStylePicker(BoardStyle style) {
         this.style = style;
     }
 
+    /**
+     * @return  the color for the mancala pit
+     */
     @Override
     public Color setMancalaPitColor() {
         return style.setMancalaPitColor();
     }
 
+    /**
+     * @return the color for regular pits
+     */
     @Override
     public Color setPitColor() {
         return style.setPitColor();
     }
 
+    /**
+     * @return the color for the board
+     */
     @Override
     public Color setBoardColor() {
         return style.setBoardColor();
