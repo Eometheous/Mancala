745757678a973319a935d03b039c240cec3fc9bd
Eometheous <jonathan.sthomas@icloud.com>
2023-04-26 09:45:49 -0700

    added JavaDoc comments made class variable private

--- src/main/java/main/DarkModeStyle.java
+++ src/main/java/main/DarkModeStyle.java
@@ -2,23 +2,42 @@ package main;
 
 import java.awt.*;
 
+/**
+ * A dark mode style for the mancala board
+ * @author Jonathan Stewart Thomas
+ * @version 1.0.0.230426
+ */
 public class DarkModeStyle implements BoardStyle{
-    Color boardColor, mancalaPitColor, pitColor;
+    private final Color boardColor, mancalaPitColor, pitColor;
+
+    /**
+     * Sets the colors for the dark style
+     */
     public DarkModeStyle() {
         boardColor = new Color(0, 0, 0);
         mancalaPitColor = new Color(73, 73, 73);
         pitColor = new Color(73, 73, 73);
     }
+
+    /**
+     * @return  the mancalaPitColor
+     */
     @Override
     public Color setMancalaPitColor() {
         return mancalaPitColor;
     }
 
+    /**
+     * @return  the pit color
+     */
     @Override
     public Color setPitColor() {
         return pitColor;
     }
 
+    /**
+     * @return  the board color
+     */
     @Override
     public Color setBoardColor() {
         return boardColor;
