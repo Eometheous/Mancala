c8f53c3fb088413ad4e11158c9d04cdc0905df5d
Eometheous <jonathan.sthomas@icloud.com>
2023-04-26 09:38:11 -0700

    Added getPits() method

--- src/main/java/main/PitsPanel.java
+++ src/main/java/main/PitsPanel.java
@@ -2,17 +2,31 @@ package main;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 /**
  * This class stores and displays the 12 pits for the Mancala Board game in a grid layout.
  * @author Kelly Dang
- * @version 1.0.1.230420
+ * @version 1.1.0.230426
  */
 public class PitsPanel extends JPanel {
-    public PitsPanel(Model<Integer> m) {
+    private final ArrayList<Pit> pits;
+    public PitsPanel(Model<Integer> m, Color color) {
+        pits = new ArrayList<>();
         setLayout(new GridLayout(2, 6, 25, 25));
         for(int i = 0; i < 12; i++) {
-            add(new Pit(m, i));
+            Pit pit = new Pit(m, i, color);
+            pits.add(pit);
+            add(pit);
         }
+    }
+
+    /**
+     * Returns the pits that this panel holds
+     * @author Jonathan Stewart Thomas
+     * @return  the pits
+     */
+    public ArrayList<Pit> getPits() {
+        return pits;
     }
 }
