d0d548809583cdde935347a02dfe8edc3b4ba7a6
Eometheous <jonathan.sthomas@icloud.com>
2023-04-26 17:49:27 -0700

    Created KimStyle

---
+++ src/main/java/Styles/KimStyle.java
@@ -0,0 +1,45 @@
+package Styles;
+
+import java.awt.*;
+
+/**
+ * A dark mode style for the mancala board
+ * @author Jonathan Stewart Thomas
+ * @version 1.0.0.230426
+ */
+public class KimStyle implements BoardStyle{
+    private final Color boardColor, fontColor, pitColor;
+
+    /**
+     * Sets the colors for the dark style
+     */
+    public KimStyle() {
+        boardColor = new Color(255, 255, 102);
+        fontColor = new Color(0, 0, 0);
+        pitColor = new Color(50, 205, 50);
+    }
+
+    /**
+     * @return  the mancalaPitColor
+     */
+    @Override
+    public Color setFontColor() {
+        return fontColor;
+    }
+
+    /**
+     * @return  the pit color
+     */
+    @Override
+    public Color setPitColor() {
+        return pitColor;
+    }
+
+    /**
+     * @return  the board color
+     */
+    @Override
+    public Color setBoardColor() {
+        return boardColor;
+    }
+}
