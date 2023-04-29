a3b081c56b7e313ddb6ea0268446daed45f8aee2
kelly <kelly.dang70@gmail.com>
2023-04-28 17:15:38 -0700

    Added a pick-up method to add stones into the pits one by one

---
+++ src/main/java/main/Mancala.java
@@ -0,0 +1,79 @@
+package main;
+
+public class Mancala {
+    private Model<Integer> mancalaPitModel;
+    private Model<Integer> pitsModel;
+
+    public Mancala(Model<Integer> mancalaPitModel, Model<Integer> pitsModel) {
+        this.mancalaPitModel = mancalaPitModel;
+        this.pitsModel = pitsModel;
+    }
+
+    public void pickUp(int pitNum) {
+        int total = pitsModel.get(pitNum); // gets total num of stones
+        pitsModel.update(pitNum, 0); // sets num of stones in this pit to be 0
+
+        boolean crossed = false;
+        boolean crossed2 = false;
+
+        if(pitNum <= 5 && pitNum > 0) {
+            pitNum--;
+        }
+        else if(pitNum >= 6 && pitNum < 11) {
+            pitNum++;
+        }
+        else if(pitNum == 0) {
+            crossed = true;
+        }
+        else if(pitNum == 11) {
+            crossed2 = true;
+        }
+
+        for(int i = 0; i < total; i++) {
+            if(crossed == true && crossed2 == false) {
+                int big = mancalaPitModel.get(0);
+                big++;
+                mancalaPitModel.update(0, big);
+                pitNum = 6;
+                crossed = false;
+            }
+            else if(crossed2 == true) {
+                System.out.println("read");
+                int big = mancalaPitModel.get(1);
+                big++;
+                mancalaPitModel.update(1, big);
+                pitNum = 5;
+                crossed2 = false;
+            }
+            else if(pitNum <= 5 && pitNum >= 0) {
+                int updated = pitsModel.get(pitNum);
+                updated++;
+                pitsModel.update(pitNum, updated);
+                if(pitNum == 0) {
+                    crossed = true;
+                }
+                else {
+                    pitNum--;
+                }
+            }
+            else if(pitNum >= 6 && pitNum <= 11) {
+                int updated = pitsModel.get(pitNum);
+                updated++;
+                pitsModel.update(pitNum, updated);
+                if(pitNum == 11) {
+                    crossed2 = true;
+                }
+                else {
+                    pitNum++;
+                }
+            }
+            else if(crossed2 == true && crossed == false) {
+                int big = mancalaPitModel.get(1);
+                big++;
+                mancalaPitModel.update(1, big);
+                pitNum = 5;
+                crossed2 = false;
+            }
+        }
+    }
+}
