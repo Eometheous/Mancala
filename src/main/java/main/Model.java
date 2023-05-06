7c9e9a1c7c9d7a0e8a209265dd86038863826813
Eometheous <jonathan.sthomas@icloud.com>
2023-05-05 17:56:55 -0700

    Added JavaDoc comments to the Model class

--- src/main/java/main/Model.java
+++ src/main/java/main/Model.java
@@ -14,21 +14,48 @@ import java.util.ArrayList;
 public class Model <T> {
     private final ArrayList<T> dataList;
     private final ArrayList<ChangeListener> listeners;
+
+    /**
+     * Creates a model object holding an array list of data
+     * and change listeners
+     */
     public Model() {
         dataList = new ArrayList<>();
         listeners = new ArrayList<>();
     }
+
+    /**
+     * Adds data to the array list
+     * @param data  the data being added
+     */
     public void add(T data) {
         dataList.add(data);
     }
+
+    /**
+     * attaches a change listener to this model
+     * @param listener  the change listener being attached
+     */
     public void attach(ChangeListener listener) {
         listeners.add(listener);
     }
+
+    /**
+     * Updates data at a specific position in the array list
+     * @param position  the position of the data being updated
+     * @param updated   the updated data
+     */
     public void update(int position, T updated) {
         dataList.set(position, updated);
         for (ChangeListener listener : listeners) {
             listener.stateChanged(new ChangeEvent(this));
         }
     }
+
+    /**
+     * Gets the data at a specific index
+     * @param index the index the data is at
+     * @return      the data
+     */
     public T get(int index) {return dataList.get(index);}
 }
